60bff46904d7b9a74294bab346014b176ccf4682
Prakhar Dwivedi <prakhar@gmail.com>
2023-10-20 15:51:43 +0530

    Added authentication feature

--- user_login.py
+++ user_login.py
@@ -1,16 +1,49 @@
 import bcrypt
+import shelve
+import getpass
+import os
+
+
+def retreive_user(user_email, entered_password):
+	with shelve.open(f'{os.getcwd()}/user.txt', 'r') as user_file:
+		try:
+			user = user_file[user_email]
+			user_hashed_password = user.get('password')
+		except Exception as e:
+			user, user_hashed_password = None, b''
+		user_file.close()
+
+	return (user, user_hashed_password)
+
+
+def get_user_password_in_bytes(entered_password):
+	if not entered_password:
+		print("Please enter password")
+		return None
+	else:
+		return entered_password.encode('utf-8')
+
+
+def check_login_password(user, user_hashed_password, password_in_bytes):
+	if user and bcrypt.checkpw(password_in_bytes, user_hashed_password):
+		print("Login Successfull! ")
+	else:
+		print("Please enter correct email or password. ")
 
 
 if __name__ == '__main__':
 
-	user = input("Enter name: ")
-	password = input("Enter password: ")
+	user_email = input("Enter Email: ")
+	entered_password = getpass.getpass("Enter password: ")
 
-	password_in_bytes = password.encode('utf-8')
+	user, user_hashed_password = retreive_user(
+		user_email,
+		entered_password
+	)
 
-	user_password = b'Myaddiddie'
+	password_in_bytes = get_user_password_in_bytes(entered_password)
 
-	if bcrypt.checkpw(password_in_bytes, user_hashed_password):
-		print("Login Successfull! ")
+	if not password_in_bytes:
+		print("Please Enter Password. ")
 	else:
-		print("Login UnSuccessfull! ")
+		check_login_password(user, user_hashed_password, password_in_bytes)
--- user_registration.py
+++ user_registration.py
@@ -1,6 +1,13 @@
+"""
+This module is used for teacher and student registration.
+"""
 import os
 import pprint
 import bcrypt
+import shelve
+import getpass
+
+import conf
 
 
 def get_hashed_password(password):
@@ -8,26 +15,73 @@ def get_hashed_password(password):
 	return bcrypt.hashpw(password.encode(), salt)
 
 
-if __name__ == '__main__':
+def get_password():
 
-	try:
-		user_file = open(f'{os.getcwd()}/user.py', 'w+')
-		user_file.close()
-	except FileExistsError as e:
-		print(e)
+	while True:
+		password = getpass.getpass("Enter Password: ")
 
-	user_name = input("Enter name: ")
-	user_email = input("Enter email: ")
-	password = input("Enter Password: ")
+		if (len(password) > conf.PASSWORD_MINIMUM_LENGTH) and (len(password) < conf.PASSWORD_MAXIMUM_LENGTH):
+			print("Please Enter password having minimum characters 8 and maximum 15. ")
+		else:
+			break
+
+	return password
 
 
+def get_user_data(user_name, user_email, password):
 	user = {
+		'user_email': user_email, 
 		'user_name': user_name,
-		'user_email': user_email,
 		'password': get_hashed_password(password),
 		'quizes': 0,
 		'maximum_score': 0
 	}
+	return user
 
-	user_data_file = open(f'{os.getcwd()}/user.py', 'w')
-	user_data_file.write(pprint.pformat(user))
+
+def get_user_name():
+
+	while True:
+		name = input("Enter Name: ")
+		if not (name and (len(name.split()) <= 3) and name.istitle()):
+			print("Please enter correct username. ")
+		else:
+			break
+	
+	return name
+
+
+def get_user_email():
+
+	while True:
+		email = input("Enter email: ")
+
+		if not (email and email.endswith('@gmail.com')):
+			print("Please Enter correct email. ")
+		else:
+			break
+
+	return email
+
+
+def save_user_data(user):
+	with shelve.open(f'{os.getcwd()}/user.txt', 'c') as user_file:
+		user_file[user_email] = user
+		user_file.close()
+
+
+if __name__ == '__main__':
+
+	user_name = get_user_name()
+
+	user_email = get_user_email()
+
+	password = get_password()
+
+	print(user_name)
+	print(user_email)
+	print(password)
+
+	# user = get_user_data(user_name, user_email, password)
+
+	# save_user_data(user)
